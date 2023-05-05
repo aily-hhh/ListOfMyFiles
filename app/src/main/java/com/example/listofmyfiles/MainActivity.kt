af801d32615b40bd8d64190c3722c0567f7145f1
Aily-Hhh <78144867+aily-hhh@users.noreply.github.com>
2023-05-06 02:29:32 +0300

    Bottom Sheet Dialog
    new string resources, new icons (arrows)

--- app/src/main/java/com/example/listofmyfiles/MainActivity.kt
+++ app/src/main/java/com/example/listofmyfiles/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.listofmyfiles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+
+    var sortTextView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sortTextView = findViewById(R.id.sortTextView)
+        sortTextView?.setOnClickListener {
+            val bottomSheet = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+            bottomSheet.setCancelable(true)
+            bottomSheet.setContentView(view)
+            bottomSheet.show()
+        }
     }
 }
